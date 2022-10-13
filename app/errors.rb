26d2293a9bca4fff971211071374cfbcc1aa85a4
nijeesh <nijeesh4all@gmail.com>
2022-10-13 14:14:05 +0530

    added comment and minor code formatting

--- app/errors.rb
+++ app/errors.rb
@@ -3,9 +3,10 @@
 module Errors
   class PawnSimulatorError < StandardError; end
 
+  # error when there exists no such direction as given
   class InvalidDirectionError < PawnSimulatorError
     def message
-      "Direction specified is invalid"
+      'Direction specified is invalid'
     end
   end
 end
