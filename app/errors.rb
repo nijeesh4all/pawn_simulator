ec0d420030c7739ceebca3848aa91c604a72564c
nijeesh <nijeesh4all@gmail.com>
2022-10-15 13:28:54 +0530

    added few more errors

--- app/errors.rb
+++ app/errors.rb
@@ -9,4 +9,30 @@ module Errors
       'Direction specified is invalid'
     end
   end
+
+  # error when the piece color given is invalid
+  class InvalidPieceColor < PawnSimulatorError
+    def message
+      'invalid piece color given it should be either black or white'
+    end
+  end
+
+  # when the move is invalid
+  class InvalidMoveError < PawnSimulatorError
+    def message
+      'invalid move'
+    end
+  end
+
+  class CantPlacePieceError < PawnSimulatorError
+    def message
+      'cant place the piece at location given'
+    end
+  end
+
+  class PawnIsNotPlacedError < PawnSimulatorError
+    def message
+      "the pawn hasn't been placed in the board."
+    end
+  end
 end
