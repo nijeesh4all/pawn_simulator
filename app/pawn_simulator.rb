f88ffe173a8a37a725bcae8ab3749f3565a6c292
nijeesh <nijeesh4all@gmail.com>
2022-10-15 13:35:23 +0530

    added handler for errors and responses

--- app/pawn_simulator.rb
+++ app/pawn_simulator.rb
@@ -1,23 +1,30 @@
 require_relative '../app/runner_logger'
+require_relative '../app/pawn_simulator_sdt_in_adapter'
+
+require 'byebug'
 
 class PawnSimulator
 
   def run
     @live = true
     @logger = RunnerLogger.new
+    @adapter = PawnSimulatorSdtInAdapter.new
 
     @logger.info 'Type `help` for instructions on usage'
 
     while @live
       print '> '
-      input = gets.chomp.split(/\s+/)
+      input = gets.chomp.downcase.split(/\s+/)
       command, *arguments = input
-
       case command
       when 'help'
         @logger.show_help_message
       when 'exit'
         exit
+      when *RunnerLogger::COMMANDS.keys
+        @logger.handle_command_response(command) do
+          @adapter.public_send(command, *arguments)
+        end
       else
         @logger.show_unrecognized_message
       end
